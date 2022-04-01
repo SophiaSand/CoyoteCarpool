398ba8929835d66446dbbdd4864bfa6891ac3b41
DavidVallej <102696142+DavidVallej@users.noreply.github.com>
2022-04-01 16:50:28 -0700

    Create EmailPasswordActivity.js

---
+++ CarPool/EmailPasswordActivity.js
@@ -0,0 +1,27 @@
+public class EmailPasswordActivity extends Activity {
+
+    private static final String TAG = "EmailPassword";
+    // [START declare_auth]
+    private FirebaseAuth mAuth;
+    // [END declare_auth]
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // [START initialize_auth]
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+        // [END initialize_auth]
+    }
+
+    // [START on_start_check_user]
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            reload();
+        }
+    }
+    // [END on_start_check_user]
