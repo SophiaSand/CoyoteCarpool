a2ce712783ad302ee976c2d004c8393b156246c7
DavidVallej <102696142+DavidVallej@users.noreply.github.com>
2022-04-01 17:02:19 -0700

    Create MainActivity.js

---
+++ CarPool/MainActivity.js
@@ -0,0 +1,22 @@
+
+// If user has signed in successfully this allows us to get their account data including display name, email, and photo.
+
+public void getUserProfile() {
+        // [START get_user_profile]
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            // Name, email address, and profile photo Url
+            String name = user.getDisplayName();
+            String email = user.getEmail();
+            Uri photoUrl = user.getPhotoUrl();
+
+            // Check if user's email is verified
+            boolean emailVerified = user.isEmailVerified();
+
+            // The user's ID, unique to the Firebase project. Do NOT use this value to
+            // authenticate with your backend server, if you have one. Use
+            // FirebaseUser.getIdToken() instead.
+            String uid = user.getUid();
+        }
+        // [END get_user_profile]
+    }
